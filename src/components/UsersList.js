398c05bacb4376ea5d3eacf8bda89de15577c711
Jarod Kober <jarod.development@gmail.com>
2023-07-28 13:21:30 -0700

    Render List of Users * Updated the UsersList component to map over the data returned from state and render a list of users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -27,7 +27,20 @@ function UsersList() {
 		return <div>Error fetching data...</div>;
 	}
 
-	return <div>{data.length}</div>;
+	const renderedUsers = data.map((user) => {
+		return (
+			<div
+				className="mb-2 border rounded"
+				key={user.id}
+			>
+				<div className="flex p-2 justify-between items-center cursor-pointer">
+					{user.name}
+				</div>
+			</div>
+		);
+	});
+
+	return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
