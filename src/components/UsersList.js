b52d54fd0392f61583c24eafa29753e1bef16069
Jarod Kober <jarod.development@gmail.com>
2023-07-28 14:37:13 -0700

    User Creation Loading & Error States * Updated the UsersList component with local state to manage loading state specifically on the addUser call * Similar to the last commit, this is splitting the calls in this component into their own local state to avoid triggering loading/error states on the entire component through the shared user slice state

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -7,6 +7,8 @@ import Skeleton from './Skeleton';
 function UsersList() {
 	const [isLoadingUsers, setIsLoadingUsers] = useState(false);
 	const [loadingUsersError, setLoadingUsersError] = useState(null);
+	const [isCreatingUser, setIsCreatingUser] = useState(false);
+	const [creatingUserError, setCreatingUserError] = useState(null);
 
 	const dispatch = useDispatch();
 
@@ -23,7 +25,11 @@ function UsersList() {
 	}, [dispatch]);
 
 	const handleUserAdd = () => {
-		dispatch(addUser());
+		setIsCreatingUser(true);
+		dispatch(addUser())
+			.unwrap()
+			.catch((err) => setCreatingUserError(err))
+			.finally(() => setIsCreatingUser(false));
 	};
 
 	if (isLoadingUsers) {
@@ -56,7 +62,12 @@ function UsersList() {
 		<div>
 			<div className="flex flex-row justify-between m-3">
 				<h1 className="m-2 text-xl">Users</h1>
-				<Button onClick={handleUserAdd}>+ Add User</Button>
+				{isCreatingUser ? (
+					'Creating User...'
+				) : (
+					<Button onClick={handleUserAdd}>+ Add User</Button>
+				)}
+				{creatingUserError && 'Error creating user...'}
 			</div>
 			{renderedUsers}
 		</div>
