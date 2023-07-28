7a0e6396c372435e89b3f63c29094557aac3b8a9
Jarod Kober <jarod.development@gmail.com>
2023-07-28 15:47:15 -0700

    Remove Users * Created a thunk to remove a user * Updated the users slice with extra reducers for the three new thunk action types * Updated the store to export the removeUser thunk * Updated the UsersLIstItem component to include a button that removes a user by calling the removeUser thunk when clicked

--- src/components/usersListItem.js
+++ src/components/usersListItem.js
@@ -1,11 +1,32 @@
-function usersListItem({ user }) {
+import { GoTrash } from 'react-icons/go';
+import Button from './Button';
+import { removeUser } from '../store';
+import { useThunk } from '../hooks/use-thunk';
+
+function UsersListItem({ user }) {
+	const [doRemoverUser, isLoading, error] = useThunk(removeUser);
+
+	const handleClick = () => {
+		doRemoverUser(user);
+	};
+
 	return (
 		<div className="mb-2 border rounded">
 			<div className="flex p-2 justify-between items-center cursor-pointer">
-				{user.name}
+				<div className="flex flex-row items-center justify-between">
+					<Button
+						className="mr-3"
+						loading={isLoading}
+						onClick={handleClick}
+					>
+						<GoTrash />
+					</Button>
+					{error && <div>Error deleting user.</div>}
+					{user.name}
+				</div>
 			</div>
 		</div>
 	);
 }
 
-export default usersListItem;
+export default UsersListItem;
