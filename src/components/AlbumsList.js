ca83c27c9ba068a10ac63c6fbdadf1ec73b658af
Jarod Kober <jarod.development@gmail.com>
2023-08-24 14:48:34 -0700

    Fixed AlbumsList Skeleton Bug * Added missing classnames so the skeleton elements display as expected

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -14,7 +14,12 @@ function AlbumsList({ user }) {
 
 	let content;
 	if (isLoading) {
-		content = <Skeleton times={3} />;
+		content = (
+			<Skeleton
+				className="h-10 w-full"
+				times={3}
+			/>
+		);
 	} else if (error) {
 		content = <div>Error loading albums.</div>;
 	} else {
