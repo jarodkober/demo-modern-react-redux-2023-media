340a211f02b4adf9e106cd71cfa959ef2ece9ba3
Jarod Kober <jarod.development@gmail.com>
2023-08-24 15:42:16 -0700

    Updated AlbumsList Skeleton Handling * Updated the AlbumsList skeleton so it renders on fetching instead of loading, so that it appears following adds and deletes in addition to initial load

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -4,7 +4,7 @@ import Button from './Button';
 import Skeleton from './Skeleton';
 
 function AlbumsList({ user }) {
-	const { data, error, isLoading } = useFetchAlbumsQuery(user);
+	const { data, error, isFetching } = useFetchAlbumsQuery(user);
 
 	const [addAlbum, results] = useAddAlbumMutation();
 
@@ -13,7 +13,7 @@ function AlbumsList({ user }) {
 	};
 
 	let content;
-	if (isLoading) {
+	if (isFetching) {
 		content = (
 			<Skeleton
 				className="h-10 w-full"
